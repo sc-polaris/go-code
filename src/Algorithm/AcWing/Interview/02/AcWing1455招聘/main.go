ea4dfb8b94da7e9a36fe425c9cd203852839c203
Liang <397548123@qq.com>
2022-03-04 00:47:06 +0800

    Acwing1455

--- src/Algorithm/AcWing/Interview/02/AcWing1455招聘/main.go
+++ src/Algorithm/AcWing/Interview/02/AcWing1455招聘/main.go
@@ -16,7 +16,14 @@ func main() {
         fmt.Scanf("%d %d", &n, &m)
 
 		for i := 0; i < m; i++ {
-			
+			fmt.Scanf("%d", &a[i])
 		}
+
+		res := 0
+		for i := 2; i <= n; i++ {
+			res = (res + a[(n-2)%m]) % i
+		}
+
+		fmt.Println(res)
 	}
 }
