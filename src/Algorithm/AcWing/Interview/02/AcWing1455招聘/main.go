18e69c4e96eda1ee9fadbc0579db29c563412839
Liang <397548123@qq.com>
2022-03-04 00:53:45 +0800

    AcWing1455

--- src/Algorithm/AcWing/Interview/02/AcWing1455招聘/main.go
+++ src/Algorithm/AcWing/Interview/02/AcWing1455招聘/main.go
@@ -2,18 +2,32 @@ package main
 
 import "fmt"
 
+/*
+约瑟夫环递推公式：
+    f(1) = 0;     //表示最后一轮的胜出者当前编号是0
+    f(x) = (f(x - 1) + m) % x , 1 < x <= n //每一轮都找到胜出者在上一轮中的编号
+    不过本题里m是在变化的，所以要相应地变为：
+==> f(x) = (f(x - 1) + a[(n - x) % m]) % x, 1 < x <= n
+*/
+
+/*
+当只有一个人时，答案res = 0，我们从两个人开始递推，这里需要注意我们的真实操作从
+n个人变为n-1个人用的是a[0]，从n-1个人变为n-2个人我们用的是a[1]，则从2个人变为1
+个人我们用的是a[(n-2)%m]
+*/
+
 const N int = 1010
 
 var (
 	n, m, T int
-	a [N]int
+	a       [N]int
 )
 
 func main() {
 	fmt.Scanf("%d", &T)
 
 	for ; T > 0; T-- {
-        fmt.Scanf("%d %d", &n, &m)
+		fmt.Scanf("%d %d", &n, &m)
 
 		for i := 0; i < m; i++ {
 			fmt.Scanf("%d", &a[i])
@@ -21,7 +35,7 @@ func main() {
 
 		res := 0
 		for i := 2; i <= n; i++ {
-			res = (res + a[(n-2)%m]) % i
+			res = (res + a[(n-i)%m]) % i
 		}
 
 		fmt.Println(res)
