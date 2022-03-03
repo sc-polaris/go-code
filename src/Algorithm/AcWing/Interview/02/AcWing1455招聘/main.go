66e719d4c08468b20d23c33f519f84f6ab98defa
Liang <397548123@qq.com>
2022-03-04 00:44:58 +0800

    AcWing1455

---
+++ src/Algorithm/AcWing/Interview/02/AcWing1455招聘/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const N int = 1010
+
+var (
+	n, m, T int
+	a [N]int
+)
+
+func main() {
+	fmt.Scanf("%d", &T)
+
+	for ; T > 0; T-- {
+        fmt.Scanf("%d %d", &n, &m)
+
+		for i := 0; i < m; i++ {
+			
+		}
+	}
+}
