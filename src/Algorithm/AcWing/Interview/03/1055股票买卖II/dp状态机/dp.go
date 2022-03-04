9d9d1f2fd796fee50c0c4294c6173a963f02fe54
Liang <397548123@qq.com>
2022-03-05 03:34:19 +0800

    AcWing 1055. 股票买卖 II dp状态机

---
+++ src/Algorithm/AcWing/Interview/03/1055股票买卖II/dp状态机/dp.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+/*
+dp状态机：
+	f[i][j]：
+		集合：考虑前i天的股市，第j天手中持股状态为j（j=0未持股、j=1持股）的方案
+		属性：方案的收益最大值
+1. 当前处于未持股状态0：
+对应可以进行的转换：
+    0->0 （不买入，继续观望，那么就什么都不发生）
+    0->1 （买入股票，那么收益就要减去当前市场的股票价格）
+2. 当前处于持股状态1：
+对应可以进行的转换：
+    1->1 （不卖出，继续观望，那么就什么都不发生）
+    1->0 （卖出股票，那么收益就要加上当前市场的股票价格）
+*/
+
+const (
+	N   = 1e5 + 10
+	INF = 0x3f3f3f3f
+)
+
+var (
+	in = bufio.NewReader(os.Stdin)
+	ot = bufio.NewWriter(os.Stdout)
+	n  int
+	w  [N]int
+	f  [N][2]int
+)
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	defer ot.Flush()
+
+	fmt.Fscan(in, &n)
+
+	for i := 1; i <= n; i++ {
+		fmt.Fscan(in, &w[i])
+	}
+
+	f[0][1] = -INF
+	for i := 1; i <= n; i++ {
+		f[i][0] = max(f[i-1][0], f[i-1][1]+w[i])
+		f[i][1] = max(f[i-1][1], f[i-1][0]-w[i])
+	}
+
+	fmt.Println(f[n][0])
+}
