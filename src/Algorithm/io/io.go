e2b85977690bf49113fb079ea670630b3f4feaa0
Liang <397548123@qq.com>
2022-03-05 02:22:55 +0800

    go-io-模版

---
+++ src/Algorithm/io/io.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	ot = bufio.NewWriterSize(os.Stdout, int(1e6))
+	in = bufio.NewScanner(os.Stdin)
+)
+
+func init()        { in.Split(bufio.ScanWords); in.Buffer(make([]byte, 4096), int(1e9)) }
+func rnS() string  { in.Scan(); return in.Text() }
+func rnI() int     { i, _ := strconv.Atoi(rnS()); return i }
+func rnF() float64 { f, _ := strconv.ParseFloat(rnS(), 64); return f }
+
+func rsI(l, r int) []int {
+	t := make([]int, r)
+	for i := l; i < r; i++ {
+		t[i] = rnI()
+	}
+	return t
+}
+
+func main() {
+
+}
