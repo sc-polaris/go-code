0218f68eb0c2c49ee79987593a02f555481b4ddc
Liang <397548123@qq.com>
2022-03-05 03:39:40 +0800

    AcWing 1055. 股票买卖

--- src/Algorithm/AcWing/Interview/03/1055股票买卖II/main.go
+++ src/Algorithm/AcWing/Interview/03/1055股票买卖II/main.go
@@ -10,7 +10,7 @@ const N = 1e5 + 10
 
 var (
 	n  int
-	a  [N]int
+	w  [N]int
 	in = bufio.NewReader(os.Stdin)
 	ot = bufio.NewWriter(os.Stdout)
 )
@@ -28,12 +28,12 @@ func main() {
 	fmt.Fscan(in, &n)
 
 	for i := 0; i < n; i++ {
-		fmt.Fscan(in, &a[i])
+		fmt.Fscan(in, &w[i])
 	}
 
 	var res int
 	for i := 1; i < n; i++ {
-		res += max(0, a[i]-a[i-1])
+		res += max(0, w[i]-w[i-1])
 	}
 
 	fmt.Fprintln(ot, res)
