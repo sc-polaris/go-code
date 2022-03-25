a5850fe87d30ffc53284abcee75e7c930fb2fac5
liangzhuang <397548123@qq.com>
2022-03-26 00:12:23 +0800

    LeetCode1115交替打印FooBar

---
+++ src/Concurrency/LeetCode1115交替打印FooBar/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var ch1 = make(chan int)
+var ch2 = make(chan int)
+var n = 3 // 打印次数
+
+func first() {
+	for i := 0; i < n; i++ {
+		<-ch2
+		fmt.Println("foo")
+		ch1 <- 1
+	}
+}
+
+func second() {
+	for i := 0; i < n; i++ {
+		<-ch1
+		fmt.Println("bar")
+		ch2 <- 1
+	}
+}
+
+func main() {
+	go first()
+	go second()
+	ch2 <- 1                // 2个线程启动后才开始输出
+	time.Sleep(time.Second) // 防止主线程退出后，子线程没运行完毕
+}
