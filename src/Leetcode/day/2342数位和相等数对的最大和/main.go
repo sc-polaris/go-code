b8da39599d17413d2e84050d2545c1a196834b7b
kerwin <48613389+mondi-zz@users.noreply.github.com>
2023-11-18 10:04:33 +0800

    leetcode

---
+++ src/Leetcode/day/2342数位和相等数对的最大和/main.go
@@ -0,0 +1,17 @@
+package _342数位和相等数对的最大和
+
+func maximumSum(nums []int) int {
+	ans := -1
+	maxV := make([]int, 82) // 最多9个9相加
+	for _, v := range nums {
+		s := 0 // 数位和
+		for x := v; x > 0; x /= 10 {
+			s += x % 10
+		}
+		if maxV[s] > 0 { // 说明左边也有数位和等于s的元素
+			ans = max(ans, maxV[s]+v) // 更新最大值
+		}
+		maxV[s] = max(maxV[s], v) // 维护数位和最大的
+	}
+	return ans
+}
