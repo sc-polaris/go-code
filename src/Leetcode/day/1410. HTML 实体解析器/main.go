e616e2ae5b1449aa743e67a86c10f5cad7446310
kerwin <48613389+mondi-zz@users.noreply.github.com>
2023-11-23 09:51:41 +0800

    leetcode

---
+++ src/Leetcode/day/1410. HTML 实体解析器/main.go
@@ -0,0 +1,36 @@
+package _410__HTML_实体解析器
+
+import "strings"
+
+func entityParser(text string) string {
+	entityMap := map[string]string{
+		"&quot;":  "\"",
+		"&apos;":  "'",
+		"&gt;":    ">",
+		"&lt;":    "<",
+		"&frasl;": "/",
+		"&amp;":   "&",
+	}
+
+	n := len(text)
+	var res strings.Builder
+	for i := 0; i < n; {
+		isEntity := false
+		if text[i] == '&' {
+			for k, v := range entityMap {
+				if i+len(k) <= n && text[i:i+len(k)] == k {
+					res.WriteString(v)
+					isEntity = true
+					i += len(k)
+					break
+				}
+			}
+		}
+		if !isEntity {
+			res.WriteByte(text[i])
+			i++
+		}
+	}
+
+	return res.String()
+}
