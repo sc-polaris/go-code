3a66084700bbd10a8424b86f388e055c5875d85b
mondi <397548123@qq.com>
2024-01-18 19:53:12 +0800

    leetcode 2171. 拿出最少数目的魔法豆

---
+++ src/Leetcode/day/202401/2171/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"slices"
+)
+
+func minimumRemoval(beans []int) int64 {
+	slices.Sort(beans)
+	sum, mx := 0, 0
+	for i, v := range beans {
+		sum += v
+		mx = max(mx, (len(beans)-i)*v)
+	}
+	return int64(sum - mx)
+}
+
+func main() {
+
+}
