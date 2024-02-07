eadd2758660ade81f6333b42304b9212e0fe41bf
sc-polaris <397548123@qq.com>
2024-02-07 10:18:34 +0800

    leetcode 2641. 二叉树的堂兄弟节点 II

---
+++ src/Leetcode/day/202402/2641/main.go
@@ -0,0 +1,54 @@
+package main
+
+// bfs 遍历二叉树
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func replaceValueInTree(root *TreeNode) *TreeNode {
+	root.Val = 0
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		tmp := q
+		q = nil
+
+		// 计算下一层节点值的和
+		nextLevelSum := 0
+		for _, node := range tmp {
+			if node.Left != nil {
+				q = append(q, node.Left)
+				nextLevelSum += node.Left.Val
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+				nextLevelSum += node.Right.Val
+			}
+		}
+
+		// 再次遍历，更新下一层节点的值
+		for _, node := range tmp {
+			childrenSum := 0 // node 左右儿子节点之和
+			if node.Left != nil {
+				childrenSum += node.Left.Val
+			}
+			if node.Right != nil {
+				childrenSum += node.Right.Val
+			}
+			// 更新 node 左右儿子节点的节点值
+			if node.Left != nil {
+				node.Left.Val = nextLevelSum - childrenSum
+			}
+			if node.Right != nil {
+				node.Right.Val = nextLevelSum - childrenSum
+			}
+		}
+	}
+	return root
+}
+
+func main() {
+
+}
