f1eff30eae55e781bf03caeddb1098720529720f
polaris <397548123@qq/com>
2024-02-25 18:37:29 +0800

    leetcode 2476. 二叉搜索树最近节点查询

---
+++ src/Leetcode/day/202402/2476/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"slices"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func closestNodes(root *TreeNode, queries []int) [][]int {
+	var a []int
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Left)
+		a = append(a, node.Val)
+		dfs(node.Right)
+	}
+	dfs(root)
+
+	ans := make([][]int, len(queries))
+	for i, q := range queries {
+		mn, mx := -1, -1
+		j, ok := slices.BinarySearch(a, q)
+		if j < len(a) {
+			mx = a[j]
+		}
+		if !ok { // a[j] > q, a[j-1] < q
+			j--
+		}
+		if j >= 0 {
+			mn = a[j]
+		}
+		ans[i] = []int{mn, mx}
+	}
+	return ans
+}
+
+func main() {
+
+}
