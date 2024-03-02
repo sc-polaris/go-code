65dd8cf9305519a643d6cc9b989c6a8b98d52316
polaris <397548123@qq/com>
2024-03-02 21:50:42 +0800

    leetcode 2368. 受限条件下可到达节点的数目

---
+++ src/Leetcode/day/202403/2368/main.go
@@ -0,0 +1,31 @@
+package main
+
+func reachableNodes(n int, edges [][]int, restricted []int) (ans int) {
+	r := make(map[int]bool, len(restricted))
+	for _, x := range restricted {
+		r[x] = true
+	}
+	g := make([][]int, n)
+	for _, e := range edges {
+		x, y := e[0], e[1]
+		if !r[x] && !r[y] {
+			g[x] = append(g[x], y)
+			g[y] = append(g[y], x)
+		}
+	}
+	var dfs func(int, int)
+	dfs = func(x, fa int) {
+		ans++
+		for _, y := range g[x] {
+			if y != fa {
+				dfs(y, x)
+			}
+		}
+	}
+	dfs(0, -1)
+	return
+}
+
+func main() {
+
+}
