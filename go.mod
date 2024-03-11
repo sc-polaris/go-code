f68ee069e17ab0d2b046359f725be4ebbccb595c
polaris <397548123@qq/com>
2024-03-11 21:09:36 +0800

    leetcode 2129. 将标题首字母大写

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-code
 
 go 1.22
 
-require github.com/emirpasic/gods v1.18.1 // indirect
+require (
+	github.com/emirpasic/gods v1.18.1 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ src/Leetcode/day/202403/2129/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strings"
+)
+
+func capitalizeTitle(title string) string {
+	a := strings.Split(title, " ")
+	for i, s := range a {
+		s := strings.ToLower(s)
+		if len(s) > 2 {
+			s = strings.Title(s)
+		}
+		a[i] = s
+	}
+	return strings.Join(a, " ")
+}
+
+func main() {
+
+}
