6f92f6a7abb56a2504e3b8e85ab8a0773303e03b
polaris <397548123@qq/com>
2024-03-13 21:29:26 +0800

    leetcode 2864. 最大二进制奇数

---
+++ src/Leetcode/day/202403/2864/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "strings"
+
+func maximumOddBinaryNumber(s string) string {
+	cnt := strings.Count(s, "1")
+	return strings.Repeat("1", cnt-1) + strings.Repeat("0", len(s)-cnt) + "1"
+}
+
+func main() {
+
+}
